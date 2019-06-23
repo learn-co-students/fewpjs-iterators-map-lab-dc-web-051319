102471a8e8756c120c94a137dd5631206bae6b17
Peter Viss <peter.c.viss@biola.edu>
2019-06-23 11:26:37 -0400

    Done.

--- index.js
+++ index.js
@@ -14,9 +14,7 @@ const tutorials = [
 function titleCased() {
   return tutorials.map(function (tutorial) {
     tutorial = tutorial.split(" ");
-    for (let i = 0; i < tutorial.length; i++){
-    tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
-    }
+  
   return tutorial.join(' ');
   });
 }
