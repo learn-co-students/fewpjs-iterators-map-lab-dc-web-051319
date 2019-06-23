ab9671401c5df75601c2f92033c833e6a8e7f559
Your Name <you@example.com>
2019-06-23 11:36:27 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// turn all of the strings in the above array into titleCase
+function titleCased(){
+// return the result of a .MAP transformation of the tutorials array
+  return tutorials.map(tutorial => {
+    // separate every word in each string by a comma
+    tutorial = tutorial.split(' ')
+    //iterate through every word. find the first character(and seprates it from the word) of the word located at index zero and
+    // make it uppercase, then add the word back to the rest of letters
+    for (var i = 0; i < tutorial.length; i++) {
+
+      tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1)
+    }
+    // after that work has been done, .join everything back to their original strings
+    // with a comma in between each course name
+    return tutorial.join(' ')
+  })
 }
