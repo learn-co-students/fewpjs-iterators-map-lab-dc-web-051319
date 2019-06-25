4bc99ebb245cccc4b37c32e784b9e30f4a2386b7
tariqjunaid <junaidtariq_91@yahoo.com>
2019-06-25 13:32:32 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = () => tutorials.map(title => {
+    let newTitle = title.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    return newTitle.join(" ");
+});
